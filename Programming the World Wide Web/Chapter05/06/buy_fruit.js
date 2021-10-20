4115658d64d7129d03f987cdcb4b9b9b38b3659a
m66rk9t <m66rk9t@gmail.com>
2021-10-20 17:47:19 +0800

    JavaScript file

---
+++ Programming the World Wide Web/Chapter05/06/buy_fruit.js
@@ -0,0 +1,50 @@
+var ct = 0;
+
+function chk_apple() {
+
+    var dom = document.getElementById("apple");
+    var num = parseInt(dom.value);
+
+    if (dom.value.search(/^\d*&/) != 0 || (num < parseInt(dom.min) || num > parseInt(dom.max))) {
+        alert("The number of apple is invalid.\nPlease enter an integer from 0 to 99.\n");
+        return false;
+    }
+    else
+        ct += num * 59;
+
+    return false;
+}
+
+function chk_orange() {
+
+    var dom = document.getElementById("orange");
+    var num = parseInt(dom.value);
+
+    if (dom.value.search(/^\d*&/) != 0 || (num < parseInt(dom.min) || num > parseInt(dom.max))) {
+        alert("The number of orange is invalid.\nPlease enter an integer from 0 to 99.\n");
+        return false;
+    }
+    else
+        ct += num * 49;
+
+    return false;
+}
+
+function chk_banana() {
+
+    var dom = document.getElementById("banana");
+    var num = parseInt(dom.value);
+
+    if (dom.value.search(/^\d*&/) != 0 || (num < parseInt(dom.min) || num > parseInt(dom.max))) {
+        alert("The number of banana is invalid.\nPlease enter an integer from 0 to 99.\n");
+        return false;
+    }
+    else
+        ct += num * 39;
+
+    return false;
+}
+
+function total_cost() {
+    alert("Your total cost is $" + ct * 0.01 + "\n")
+}
