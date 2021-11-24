169043432d932490d5ce10818c7487798d61237c
m66rk9t <m66rk9t@gmail.com>
2021-11-24 16:49:30 +0800

    update PHP file

--- Programming the World Wide Web/Chapter09/02/avgandmid.php
+++ Programming the World Wide Web/Chapter09/02/avgandmid.php
@@ -1,25 +1,27 @@
 <?php
-	function AvgAndMid($IntArray){
-		$set = array(0, 0);
-		$total = 0;$count = 0;
-		foreach($IntArray as $val){
-			$total += $val;
-			$count++;
-		}
-		$set[0] = (double)$total / $count;
-		$set[1] = $IntArray[($count - 1) / 2];
+    function AvgAndMid($IntArray)
+    {
+        $set = array(0, 0);
+        $total = 0;
+        $count = 0;
+        foreach ($IntArray as $val) {
+            $total += $val;
+            $count++;
+        }
+        $set[0] = (float)$total / $count;
+        $set[1] = $IntArray[($count - 1) / 2];
 
-		return $set;
-	}
+        return $set;
+    }
 
-	$myArray = array(1, 2, 3, 4, 5);
-	
-	$retArray = AvgAndMid($myArray);
+    $myArray = array(1, 2, 3, 4, 5);
 
-	echo "Original Array:";
-	foreach($myArray as $val){
-		echo "$val ";
-	}
-	
-	echo "\nAverage:$retArray[0]\nMiddle Value:$retArray[1]";
+    $retArray = AvgAndMid($myArray);
+
+    echo "Original Array:";
+    foreach ($myArray as $val) {
+        echo "$val ";
+    }
+
+    echo ",Average:$retArray[0],Middle Value:$retArray[1]";
 ?>
--- Programming the World Wide Web/Chapter09/04/boundary.php
+++ Programming the World Wide Web/Chapter09/04/boundary.php
@@ -1,24 +1,25 @@
 <?php
-	function Boundary($arr, &$smaller, &$greater){
-		$sindex = 0;$gindex = 0;
-		foreach($arr as $val){
-			if ($val < 0)
-				$smaller[$sindex++] = $val;
-			if ($val > 0)
-				$greater[$gindex++] = $val;
-		}
-	
-	}
+    function Boundary($arr, &$smaller, &$greater)
+    {
+        $sindex = 0;
+        $gindex = 0;
+        foreach ($arr as $val) {
+            if ($val < 0)
+                $smaller[$sindex++] = $val;
+            if ($val > 0)
+                $greater[$gindex++] = $val;
+        }
+    }
 
-	$myArr = array(-5, -4, -3, 0, 6, 7, 8);
-	$sArr;$gArr;
+    $myArr = array(-5, -4, -3, 0, 6, 7, 8);
+    $sArr;
+    $gArr;
 
-	Boundary($myArr, $sArr, $gArr);
-	
-	foreach($sArr as $val1)
-		echo "$val1";
-	print "\n";
-	foreach($gArr as $val2)
-		echo "$val2";
+    Boundary($myArr, $sArr, $gArr);
 
+    foreach ($sArr as $val1)
+        echo "$val1";
+    print "\n";
+    foreach ($gArr as $val2)
+        echo "$val2";
 ?>
