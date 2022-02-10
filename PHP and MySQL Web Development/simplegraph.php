b85ce34269a2ab66b2ca7d0eb8fca6b9a1996489
m66rk9t <m66rk9t@gmail.com>
2022-02-10 11:46:07 +0800

    Upload simplegraph.php

---
+++ PHP and MySQL Web Development/simplegraph.php
@@ -0,0 +1,21 @@
+<?php
+//创建画布
+$height = 200;
+$width = 200;
+$im = imagecreatetruecolor($width, $height);
+$white = imagecolorallocate($im, 255, 255, 255,);
+$blue = imagecolorallocate($im, 0, 0, 255,);
+
+//绘制图像
+imagefill($im, 0, 0, $blue);
+imageline($im, 0, 0, $width, $height, $white);
+imagestring($im, 4, 50, 150, 'Sales', $white);
+
+//输出图像
+header("Content-Type: image/png");
+imagepng($im);//调用此函数后，会将图像输出至浏览器
+imagepng($im, "L:/Website/myfitst.png");//调用此函数后，不会将图像输出至浏览器，会保存图像至指定位置
+
+//清空资源
+imagedestroy($im);
+?>
